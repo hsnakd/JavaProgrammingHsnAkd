a5ed8b69a4986be4bddb52dbb02d82301b72c772
hsnakd <hsnakd10@gmail.com>
2022-07-28 03:56:06 +0300

    day 35 tasks

--- src/day35_Encapsulation/Z_5_PizzaTask/PizzaObject.java
+++ src/day35_Encapsulation/Z_5_PizzaTask/PizzaObject.java
@@ -2,7 +2,7 @@ package day35_Encapsulation.Z_5_PizzaTask;
 
 public class PizzaObject {
     public static void main(String[] args) {
-        PizzaClass pizza1 = new PizzaClass("medium", 2,3);
+        PizzaClass pizza1 = new PizzaClass("large", 2,3);
         System.out.println(pizza1);
     }
 }
