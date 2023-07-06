ccc96c511ec3c9df12050c9e6452b7fb2ac7fe00
hsnakd <hsnakd10@gmail.com>
2023-07-07 01:03:37 +0300

    Fast Track Java Day 1-2-3 Package name error fixed

--- src/fastTrackJava/day1/Task5FrequencyOfCharacter.java
+++ src/fastTrackJava/day1/Task5FrequencyOfCharacter.java
@@ -1,5 +1,9 @@
 package fastTrackJava.day1;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task5FrequencyOfCharacter {
 
     /*
@@ -27,5 +31,33 @@ public class Task5FrequencyOfCharacter {
 
         System.out.println(result);
 
+
+
+
+        System.out.println("-----------------------------------------------------------");
+
+
+            String input = "aaaabcccddeeeeffzzzzz";
+            String output = getCharacterFrequency(input);
+            System.out.println(output);
+
     }
+        public static String getCharacterFrequency(String input) {
+            Map<Character, Integer> frequencyMap = new HashMap<>();
+
+            // Count the frequency of each character
+            for (char c : input.toCharArray()) {
+                frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+            }
+
+            // Build the concatenated string
+            StringBuilder sb = new StringBuilder();
+            for (Map.Entry<Character, Integer> entry : frequencyMap.entrySet()) {
+                sb.append(entry.getKey()).append(entry.getValue());
+            }
+
+            return sb.toString();
+        }
+
+
 }
